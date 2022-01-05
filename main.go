a393fee99e489a0265004f4c5d8196946666dc87
Sun-Case <Sun-Case@protonmail.com>
2022-01-05 09:14:47 +0800

    Update: main.go

--- main.go
+++ main.go
@@ -1,4 +1,11 @@
 package main
 
+import "bilibili-live-communication/bilibili/ipc"
+
 func main() {
+	go ipc.TcpListen("0.0.0.0:22333")
+	go ipc.UdsListen("/tmp/bili.sock")
+	go ipc.WsListen("/", "0.0.0.0:22334")
+
+	select {}
 }
